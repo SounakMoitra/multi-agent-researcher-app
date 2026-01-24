ffa570e7ee25ea8627ccc0334cc86272fedd2f7b
Sounak Moitra <sounakmoitra04@gmail.com>
2026-01-24 19:53:19 +0530

    minor mod in search agent

--- backend/app/agents/search_agent.py
+++ backend/app/agents/search_agent.py
@@ -1,11 +1,7 @@
-import os 
-from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.tools.tavily_search import TavilySearchResults
 
-
-load_dotenv()
 
 
 class SearchAgent:
